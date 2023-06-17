669df6170eb2fa34b0f9e0c1ac8f6c794181854e
unknown <muhammadfirdausabdullah2010@gmail.com>
2023-06-17 22:52:27 +0700

    study case

---
+++ index.js
@@ -0,0 +1,61 @@
+//ALGORITMA DAN PSEUDOCODE
+
+/**
+ * Study case 1
+ * 
+ * Buatlah Algoritma & Pseudocode untuk luas lingkaran
+ * 
+ * ALGORITMA
+ * 1. Definisikan luas lingkaran = pi * r * r
+ * 2. Buat variabel pi = 3.14
+ * 3. Buat variabel r
+ * 4. Buat variabel luas lingkaran
+ * 5. isi luas dengan rumus pi kali r kali r
+ * 6. Cetak luas lingkaran
+ * 
+ * PSEUDOCODE
+ * 1. Masukkan r dengan angka
+ * 2. Masukkan pi dengan 3.14
+ * 3. Masukkan luas dengan rumus pi kali r kali r.
+ * 4. Cetak luas lingkaran
+ * 
+ * Study case 2
+ * 
+ * Buatlah Algoritma & Pseudocode untuk volume tabung
+ * 
+ * ALGORITMA
+ * 1. Definisikan volume tabung = pi * r * r * tinggi
+ * 2. Buat variabel pi = 3.14
+ * 3. Buat variabel r
+ * 4. Buat variabel tinggi
+ * 5. buat variabel volume tabung
+ * 6. Isi volume tabung dengan rumus pi kali r kali r kali tinggi
+ * 7. Cetak volume tabung
+ * 
+ * PSEUDOCODE
+ * 1. Masukkan pi dengan angka 3.14
+ * 2. Masukkan r dengan angka
+ * 3. Masukkan tinggi dengan angka
+ * 4. Masukkan volume tabung dengan rumus pi kali r kali r kali tinggi
+ * 5. Cetak volume tabung
+ * 
+ * Study case 3
+ * 
+ * Buatlah Algoritma & Pseudocode untuk volume "Setengah bola"
+ * 
+ * ALGORITMA
+ * 1. Definisikan volume setengah bola = 2/3 * pi * r * r * r
+ * 2. Buat variabel "2/3"
+ * 3. Buat variabel pi = 3.14
+ * 4. Buat variabel r
+ * 5. Buat variabel volume setengah bola
+ * 6. Isi volume setengah bola dengan rumus 2/3 kali r kali r kali r
+ * 7. Cetak volume setengah bola
+ * 
+ * PSEUDOCODE
+ * 1. Masukkan pi dengan angka 3.14
+ * 2. Masukkan r dengan angka
+ * 3. Masukkan volume setengah bola dengan rumus 2/3 kali r kali r kali r
+ * 4. Cetak volume setengah bola
+ * 
+ */
