2058a5581055a6eedf1426d9944227f59a7a644c
unknown <muhammadfirdausabdullah2010@gmail.com>
2023-06-18 14:16:20 +0700

    Variabel data

---
+++ Variable/index.js
@@ -0,0 +1,54 @@
+// Variable dan Tipe Data
+
+//console.log("Hello World.");
+
+//Study Case
+// Buatlah algoritma dan pseudocode dan codingan dari keliling persegi panjang
+
+/**
+ * ALGORITMA
+ * 1. Definisikan keliling persegi = 2 * P + 2 * L
+ * 2. Buat variabel panjang
+ * 3. Buat variabel lebar
+ * 4. Buat variabel keliling persegi panjang
+ * 4. Isi keliling persegi panjang dengan rumus 2 kali panjang tambah lebar
+ * 5. Cetak keliling persegi panjang
+ * 
+ * PSEUDOCODE
+ * 1. Isi variabel panjang dengan angka
+ * 2. Isi variabel lebar dengan angka
+ * 3. Isi keliling persegi panjang dengan rumus 2 kali panjang tambah lebar
+ * 4. Cetak keliling persegi panjang
+ * 
+ * CODING
+ */
+// var panjang = 5;
+// var lebar = 10;
+// var keliling = 2 * panjang + 2 * lebar;
+// console.log("Keliling persegi panjang adalah ", keliling);
+
+// Study Case
+
+//Tentukan algoritma dan pseudocode dan coding dari luas permukaan tabung
+/**
+ * ALGORITMA
+ * 1. Definisikan luas permukaan tabung = 2 *(pi * r * r)
+ * 2. Buat variabel pi = 3.14
+ * 3. Buat variabel r = 6
+ * 4. Buat variabel luas permukaan tabung
+ * 5. Isi luas permukaan dengan rumus 2 kali pi kali r kali r
+ * 6. Cetak luas permukaan tabung
+ * 
+ * PSEUDOCODE
+ * 1. Isi variabel pi dengan 3.14
+ * 2. Isi variabel r dengan 6
+ * 3. Isi luas permukaan dengan rumus 2 kali pi kali r kali r
+ * 4. Cetak luas permukaan tabung
+ * 
+ * CODING
+ */
+var pi = 3.14;
+var r = 6;
+var luasPermukaan = 2 * (pi * r * r);
+
+console.log('Luas Permukaan Tabung adalah', luasPermukaan);
